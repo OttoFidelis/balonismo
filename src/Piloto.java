25bb6b438fcf0bba8a969cbf34eb461a8904083f
OttoFidelis <131968694+OttoFidelis@users.noreply.github.com>
2023-07-10 11:10:27 -0300

    Update Piloto.java

--- src/Piloto.java
+++ src/Piloto.java
@@ -1,28 +1,26 @@
 public class Piloto {
-    private long cpf;
+    private String cpf;
     private long n_anac;
-    public long getCpf() {
-        return cpf;
+
+    public String getCpf() {
+        return this.cpf;
     }
 
-    public void setCpf(long cpf) {
+    public void setCpf(String cpf) {
         this.cpf = cpf;
     }
 
     public long getN_anac() {
-        return n_anac;
+        return this.n_anac;
     }
 
     public void setN_anac(long n_anac) {
         this.n_anac = n_anac;
     }
 
-    long n_anac;
+    Piloto(String cpf, long n_anac) {
+        this.cpf = cpf;
+        this.n_anac = n_anac;
+    }
 }
-Piloto(
-        long cpf,
-        long n_anac
-){
-    this.cpf=cpf;
-    this.n_anac=n_anac;
-}
+
