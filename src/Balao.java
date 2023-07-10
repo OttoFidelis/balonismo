20732bf9bd76b409c61a553a55b55decca2478f8
OttoFidelis <131968694+OttoFidelis@users.noreply.github.com>
2023-07-10 11:08:31 -0300

    Update Balao.java

--- src/Balao.java
+++ src/Balao.java
@@ -25,5 +25,23 @@ public class Balao {
         this.regis_regularidade=regis_regularidade;
     }
 
+    public int getNumero_id() {
+        return numero_id;
+    }
 
+    public void setNumero_id(int numero_id) {
+        this.numero_id = numero_id;
+    }
+
+    public int getRegis_regularidade() {
+        return regis_regularidade;
+    }
+
+    public void setRegis_regularidade(int regis_regularidade) {
+        this.regis_regularidade = regis_regularidade;
+    }
+    Balao( int numero_id, int regis_regularidade){
+        this.numero_id = numero_id;
+        this.regis_regularidade = regis_regularidade;
+    }
 }
