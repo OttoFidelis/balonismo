f32b7a599b48ed9e8e50ad90d7c649992de73cb4
OttoFidelis <ottofidelis@gmail.com>
2023-07-14 19:04:55 -0300

    Agora o troço tá funcionando MESMO, eu acho

--- src/Balao.java
+++ src/Balao.java
@@ -16,30 +16,6 @@ public class Balao {
     public void setRegis_regularidade(int regis_regularidade) {
         this.regis_regularidade = regis_regularidade;
     }
-
-    Balao(
-            int numero_id,
-            int regis_regularidade
-    ){
-        this.numero_id=numero_id;
-        this.regis_regularidade=regis_regularidade;
-    }
-
-    public int getNumero_id() {
-        return numero_id;
-    }
-
-    public void setNumero_id(int numero_id) {
-        this.numero_id = numero_id;
-    }
-
-    public int getRegis_regularidade() {
-        return regis_regularidade;
-    }
-
-    public void setRegis_regularidade(int regis_regularidade) {
-        this.regis_regularidade = regis_regularidade;
-    }
     Balao( int numero_id, int regis_regularidade){
         this.numero_id = numero_id;
         this.regis_regularidade = regis_regularidade;
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
     public Main() {
     }
 
     public static void main(String[] args) {
-        new Pessoa("Gerialdo", "889.456.345-23", "945563254", 954466576L, 989399409L, "rua geriátrica, 64, Telescópolis", "Termo assinado");
-        new Pessoa("Childete", "386.586.490-27", "939857486", 954466576L, 989399409L, "rua geriátrica, 64, Telescópolis", "Termo assinado");
-        new Piloto("398.889.289-98", 298L);
-        new Balao(12, 321);
+       Pessoa p1 = new Pessoa ("Gerialdo", "889.456.345-23", "945563254", 954466576L, 989399409L, "rua geriátrica, 64, Telescópolis", "Termo assinado");
+       Pessoa p2 = new Pessoa("Childete", "386.586.490-27", "939857486", 954466576L, 989399409L, "rua geriátrica, 64, Telescópolis", "Termo assinado");
+       Piloto pilot = new Piloto("398.889.289-98", 298L);
+       Balao balao = new Balao(12, 321);
     }
+
 }
